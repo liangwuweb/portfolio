500b107c5144d175f20a0e054f325810bff19aed
Liang Wu <44330488+liangwuweb@users.noreply.github.com>
2020-01-14 16:18:13 -0600

    upload new files from Wu_Liang_devfile

---
+++ js/custom_mobile.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  $('.sandwich-bar').on('tap',function(event){
+    event.preventDefault();
+    if($("#nav").is(":visible")){
+       $("#nav").fadeOut(500);
+    } else {
+      $("#nav").fadeIn(500);
+    }
+  });
+
+  $(".nav-item").on('tap', function(event) {
+    event.preventDefault();
+    var destination = $(this).attr('href');
+    pageScrollInProgress = true;
+    $('html, body').stop().animate({
+      scrollTop: $(destination).offset().top - (-1)
+    }, 900, 'easeInOutExpo', function() {
+      pageScrollInProgress = false;
+    });
+
+    
+  });
+
+  $(window).resize(function() {
+    if ($(window).width() >= 768) {
+      $("#nav").css("display", "block");
+    } else {
+      $("#nav").css("display", "none")
+    }
+  });
+});
